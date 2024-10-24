7369dc20fd9c4eceb7a0216dbf1c0e838f4d89be
Nikolay Michel <61086458+NikolayTheSTAR@users.noreply.github.com>
2024-10-24 23:53:07 +0300

    Fire End

--- Assets/Scripts/World/Player.cs
+++ Assets/Scripts/World/Player.cs
@@ -1,9 +1,24 @@
 using System;
 using UnityEngine;
+using World;
+using DG.Tweening;
 
 public class Player : Creature, ICameraFocusable, IKeyInputHandler
 {    
     [SerializeField] private GameObject fire;
+    [SerializeField] private EntranceTrigger trigger;
+
+    private Tweener slowFireEndTweener;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        trigger.Init(OnEnter, OnExit);
+    }
 
     #region KeyInput
 
@@ -19,4 +34,28 @@ public class Player : Creature, ICameraFocusable, IKeyInputHandler
     {}
 
     #endregion
+
+    private void OnEnter(Collider col)
+    {
+        if (col.CompareTag("Window"))
+        {
+            slowFireEndTweener =
+            DOVirtual.Float(0f, 1f, 5f, (temp) => {}).OnComplete(() =>
+            {
+                fire.SetActive(false);
+            });
+        }
+        else if (col.CompareTag("Drops"))
+        {
+            fire.SetActive(false);
+        }
+    }
+
+    private void OnExit(Collider col)
+    {
+        if (col.CompareTag("Window"))
+        {
+            slowFireEndTweener?.Kill();
+        }
+    }
 }
